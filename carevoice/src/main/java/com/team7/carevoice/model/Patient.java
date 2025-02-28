480269ff5d939fe0750260b3c4a575d51d396f8d
colecathcart <cole.cathcart1@ucalgary.ca>
2025-02-27 17:13:15 -0700

    Patient model

---
+++ carevoice/src/main/java/com/team7/carevoice/model/Patient.java
@@ -0,0 +1,60 @@
+package com.team7.carevoice.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Patient {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Automatically generated unique ID
+    private Long id;
+
+	@Column(nullable = false, unique = true) // Ensure AHS number is unique and non-null
+    private String ahsNumber;
+    private String name;
+    private Date dateOfBirth;
+
+	public Patient() {
+
+	}
+
+	public Patient(String ahsNumber, String name, Date dateOfBirth) {
+		this.ahsNumber = ahsNumber;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getAhsNumber() {
+		return ahsNumber;
+	}
+
+	public void setAhsNumber(String ahsNumber) {
+		this.ahsNumber = ahsNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+}
