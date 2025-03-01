09e93f571d342241b64581428a75c607d06b3fbd
Xin Wang <wxworooo@outlook.com>
2025-02-28 21:20:32 -0700

    Add Post

--- carevoice/src/main/java/com/team7/carevoice/controller/TranscriptController.java
+++ carevoice/src/main/java/com/team7/carevoice/controller/TranscriptController.java
@@ -1,6 +1,8 @@
 package com.team7.carevoice.controller;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -80,4 +82,5 @@ public class TranscriptController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
+
 }
